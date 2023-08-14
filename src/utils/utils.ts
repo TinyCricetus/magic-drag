dbdb9cb94f075ba5171a8c088e24e69de25caad1
仓鼠 <tinycricetus@qq.com>
2023-08-14 09:12:59 +0800

    feat: remove useless function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,23 +7,6 @@ export function makeBoxObserver(el: HTMLElement, callback: () => void) {
   observer.observe(el)
 }
 
-export function useRequestAnimationFrame(callback: (timestamp: number) => void) {
-  let animationFrameId = -1
-
-  const cancel = () => {
-    window.cancelAnimationFrame(animationFrameId)
-  }
-
-  const start = () => {
-    cancel()
-    animationFrameId = window.requestAnimationFrame((time) => {
-      callback(time)
-    })
-  }
-
-  return [start, cancel] as const
-}
-
 export function throttle<T>(fn: (arg: T) => void, time: number) {
   let lastTime = 0
   return (arg: T) => {
